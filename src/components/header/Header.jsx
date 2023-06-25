37e40a63459bcceeaad14ba79d9cdd5ac4194cc4
StefTih <stefanovtih02@gmail.com>
2023-06-25 19:28:38 +0100

    Altered some files

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello my name is</h5>
         <h1>Tihomir Stefanov</h1>
-        <h5 className='text-light'>Fullstack Developer</h5>
+        <h5 className='text-light'>Junior Software Engineer</h5>
         <CTA/>
         <div className='my_image'>
           <img src={MyImage} alt="my_imag" />
